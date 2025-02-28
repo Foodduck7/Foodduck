85a569d04d73c06e3a9f8ad5b1dc83f6cb6abddc
sjMun09 <ohoh7391@naver.com>
2025-02-28 14:20:17 +0900

    init

---
+++ src/main/java/com/example/foodduck/FoodduckApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodduckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodduck/FoodduckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodduckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

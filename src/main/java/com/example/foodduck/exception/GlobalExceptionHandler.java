ee7fcda83571db0c5f37d64ec424c725c7565ca4
abc <clvlajrrhtlvek@gmail.com>
2025-03-04 13:18:25 +0900

    feat(exception): Valid 예외처리 추가
    Valid 유효성 검사 실패 시 실패 메세지를 출력하도록 하였습니다.

--- src/main/java/com/example/foodduck/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/foodduck/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package com.example.foodduck.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +19,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleException(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        List<String> fieldErrorList = fieldErrors.stream()
+                .map(FieldError::getDefaultMessage)  // 각 필드의 오류 메시지를 가져온다.
+                .toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(fieldErrorList);
     }
 }

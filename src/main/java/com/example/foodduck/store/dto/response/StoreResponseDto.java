ecb0e0bb602da50949a0af451b666a754fd4c566
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-03-04 16:33:38 +0900

    refactor: StoreResponseDto의 owner 타입을 User에서 UserResponse로 변경

--- src/main/java/com/example/foodduck/store/dto/response/StoreResponseDto.java
+++ src/main/java/com/example/foodduck/store/dto/response/StoreResponseDto.java
@@ -3,28 +3,31 @@ package com.example.foodduck.store.dto.response;
 import com.example.foodduck.store.entity.BreakState;
 import com.example.foodduck.store.entity.Store;
 import com.example.foodduck.store.entity.StoreState;
+import com.example.foodduck.user.dto.response.UserResponse;
 import com.example.foodduck.user.entity.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 
 @Getter
+@NoArgsConstructor
 public class StoreResponseDto {
 
-    private final Long id;
-    private final User owner;
-    private final String name;
-    private final int minOrderPrice;
-    private final LocalTime openTime;
-    private final LocalTime closeTime;
-    private final BreakState breakState;
-    private final int likeCount;
-    private final int orderCount;
-    private final StoreState storeState;
+    private Long id;
+    private UserResponse owner;
+    private String name;
+    private int minOrderPrice;
+    private LocalTime openTime;
+    private LocalTime closeTime;
+    private BreakState breakState;
+    private int likeCount;
+    private int orderCount;
+    private StoreState storeState;
 
     public StoreResponseDto(Store store) {
         this.id = store.getId();
-        this.owner = store.getOwner();
+        this.owner = new UserResponse(store.getOwner());
         this.name = store.getName();
         this.minOrderPrice = store.getMinOrderPrice();
         this.openTime = store.getOpenTime();

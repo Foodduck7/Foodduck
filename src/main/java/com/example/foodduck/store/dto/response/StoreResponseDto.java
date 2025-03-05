7abc45d407a7eeda427e852a468da1d5d902ae1e
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-03-05 15:13:35 +0900

    refactor(store): StoreResponseDto 필드 및 Lombok 애노테이션 수정

--- src/main/java/com/example/foodduck/store/dto/response/StoreResponseDto.java
+++ src/main/java/com/example/foodduck/store/dto/response/StoreResponseDto.java
@@ -4,26 +4,23 @@ import com.example.foodduck.store.entity.BreakState;
 import com.example.foodduck.store.entity.Store;
 import com.example.foodduck.store.entity.StoreState;
 import com.example.foodduck.user.dto.response.UserResponse;
-import com.example.foodduck.user.entity.User;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 
 @Getter
-@NoArgsConstructor
 public class StoreResponseDto {
 
-    private Long id;
-    private UserResponse owner;
-    private String name;
-    private int minOrderPrice;
-    private LocalTime openTime;
-    private LocalTime closeTime;
-    private BreakState breakState;
-    private int likeCount;
-    private int orderCount;
-    private StoreState storeState;
+    private final Long id;
+    private final UserResponse owner;
+    private final String name;
+    private final int minOrderPrice;
+    private final LocalTime openTime;
+    private final LocalTime closeTime;
+    private final BreakState breakState;
+    private final int likeCount;
+    private final int orderCount;
+    private final StoreState storeState;
 
     public StoreResponseDto(Store store) {
         this.id = store.getId();

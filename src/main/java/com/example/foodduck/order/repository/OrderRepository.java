066f920f2e926fa72478f5483beac3bdae2e0e43
yjn33 <yjn0032@gmail.com>
2025-03-06 22:29:22 +0900

    feat: 주문id로 리뷰 찾는 기능 구현
    OrderRepository.java -> findOrderById구현

--- src/main/java/com/example/foodduck/order/repository/OrderRepository.java
+++ src/main/java/com/example/foodduck/order/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.example.foodduck.order.repository;
 
+import com.example.foodduck.exception.ApplicationException;
 import com.example.foodduck.order.entity.Order;
+import org.apache.coyote.BadRequestException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
 
 /**
  * 주문 기능 쿼리를 DB로 보내 실행하는 인터페이스
@@ -9,4 +13,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @version JPA repository 를 상속받아 기본 기능 사용
  */
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    default Order findOrderById(Long id) {
+        return findById(id).orElseThrow(() -> new ApplicationException("해당 주문은 존재 안함", HttpStatus.BAD_REQUEST));
+    }
 }
